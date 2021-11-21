92e7a1750997eae4397fcc2cfc27359f1228cdd2
Bobokhuja <bobokhuja@yandex.ru>
2021-11-21 10:26:52 +0500

    игнорирование папки dist и добавление правила в stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   "extends": ["stylelint-config-standard", "stylelint-config-recess-order"],
   "rules": {
     "at-rule-empty-line-before": null,
+    "no-invalid-position-at-import-rule": null,
     "indentation": 2,
     "at-rule-no-unknown": [true, {
       "ignoreAtRules": ["import", "include", "mixin", "if", "return", "use"]
