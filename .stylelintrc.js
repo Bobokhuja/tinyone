ab61414082f9b6785167288ce157f93377baea22
Bobokhuja <bobokhuja@yandex.ru>
2021-11-21 11:09:04 +0500

    стилизация блока header

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   "rules": {
     "at-rule-empty-line-before": null,
     "no-invalid-position-at-import-rule": null,
+    "selector-class-pattern": null,
     "indentation": 2,
     "at-rule-no-unknown": [true, {
       "ignoreAtRules": ["import", "include", "mixin", "if", "return", "use"]
